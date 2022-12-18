bf386992f4b244380c7b3a4f6fad7ca4275a51b4
MorvidAngel <5d7n2s9m6b@live.com>
2022-12-18 01:22:54 -0500

    more php

---
+++ mvcproject/app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+// db parametors
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'tmvc');
+
+// app root
+define('APPROOT', dirname(dirname(__FILE__)));
+// url root
+// the last slash does matter or else you have double slashes
+define('URLROOT', 'http://localhost/oopsandbox/mvcproject');
+// site name
+define('SITENAME', 'ThisIsMyWebsite');
--- mvcproject/app/libraries/Core.php
+++ mvcproject/app/libraries/Core.php
@@ -13,11 +13,48 @@ class Core
 
     public function __construct()
     {
-        $this->getUrl();
+        // print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        // look in controllers for first value
+        // isset was added to fix bug
+        if (isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            // if exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // unset 0 index
+            unset($url[0]);
+        }
+
+        // require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // instantiate controller class
+        $this->currentController = new $this->currentController;
+
+        // check for second part of url
+        if (isset($url[1])) {
+            // check to see if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                // unset 1 index
+                unset($url[1]);
+            }
+        }
+
+        // get params
+        $this->params = $url ? array_values($url) : [];
+
+        // call a callbeck with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl()
     {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
