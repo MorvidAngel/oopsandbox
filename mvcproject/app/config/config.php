23b9eddfeacfcd3b4d86f7391e3528c8e93495cd
MorvidAngel <5d7n2s9m6b@live.com>
2022-12-19 00:02:03 -0500

    framework done

--- mvcproject/app/config/config.php
+++ mvcproject/app/config/config.php
@@ -1,16 +1,17 @@
 <?php
 // db parametors
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+// your user name
+define('DB_USER', '_YOUR_USER_');
 // the password is 1-7 not like in the video, if you change it youll get a error
-define('DB_PASS', '1234567');
+define('DB_PASS', '_YOUR_PASS_');
 // to create the tmvc youd have to go to phpadmin 
-define('DB_NAME', 'tmvc');
+define('DB_NAME', '_YOUR_DBNAME_');
 
 // app root
 define('APPROOT', dirname(dirname(__FILE__)));
 // url root
 // the last slash does matter or else you have double slashes
-define('URLROOT', 'http://localhost/oopsandbox/mvcproject');
+define('URLROOT', '_YOUR_URL_');
 // site name
-define('SITENAME', 'ThisIsMyWebsite');
+define('SITENAME', '_YOUR_SITENAME_');
--- mvcproject/app/controllers/Pages.php
+++ mvcproject/app/controllers/Pages.php
@@ -3,12 +3,11 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('post');
     }
 
     public function index()
     {
-        $data = ['title' => 'Welcome'];
+        $data = ['title' => 'MVC PHP'];
 
         $this->view('pages/index', $data);
     }
