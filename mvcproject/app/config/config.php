21a97eacfc9bfb80936805beb549559b52ce6047
MorvidAngel <5d7n2s9m6b@live.com>
2022-12-18 20:27:18 -0500

    php

--- mvcproject/app/config/config.php
+++ mvcproject/app/config/config.php
@@ -2,7 +2,9 @@
 // db parametors
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '123456');
+// the password is 1-7 not like in the video, if you change it youll get a error
+define('DB_PASS', '1234567');
+// to create the tmvc youd have to go to phpadmin 
 define('DB_NAME', 'tmvc');
 
 // app root
